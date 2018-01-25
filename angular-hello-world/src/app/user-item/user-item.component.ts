5ab321b4bc23330dcf4efe3778a42f2f1b38312d
Jason Xiong <JasonJianXiong@gmail.com>
2018-01-25 00:40:38 -0500

    no message

---
+++ angular-hello-world/src/app/user-item/user-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, 
+        OnInit,
+        Input
+       } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css']
+})
+export class UserItemComponent implements OnInit {
+  @Input()  haha:string;
+  
+  constructor() { 
+// this.name ="felix";
+  
+  }
+
+  ngOnInit() {
+  }
+
+}
